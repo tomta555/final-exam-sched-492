e30b13e87d124c1adf571abb8f41786aa91e1ec1
unknown <krit.upanun@gmail.com>
2020-10-31 13:02:52 +0700

    read regist & add func getslot by course

--- penalty_calc.py
+++ penalty_calc.py
@@ -5,6 +5,18 @@ with open('exams-timetbl.txt','r') as read_exam_slot:
     for i in read_exam_slot:
         course,slot = i.strip().split(' ')
         course_slot[course]=slot
-    x=  [key for key,val in course_slot.items() if val=='24'] 
-    print(x)
-      # mydict.keys()[mydict.values().index(16)]
+    # x=  [key for key,val in course_slot.items() if val=='24'] 
+    # print(x)
+with open('regist.in','r') as regis:
+    student = list(csv.reader(regis, delimiter=' '))
+# get_slot(course_slot,student)
+
+def get_slot(course_slot,student):
+    student_slot = []
+    for s in student:
+        each_student_slot = []
+        for c in s:
+            each_student_slot.append(int(course_slot.get(c,99)))
+        student_slot.append(each_student_slot)
+    return(student_slot)
+# print(get_slot(course_slot,student))
