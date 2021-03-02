82a649924fbb3b9e74224716ccd89c45fecc1059
Thanawong <thanawong.saneewong@gmail.com>
2021-03-03 03:31:17 +0700

    Update sched algorithm

--- final_exam_graph_coloring_NSC.py
+++ final_exam_graph_coloring_NSC.py
@@ -1,42 +1,119 @@
+import os
 import sys
 import csv
 import random
 import time
 import itertools
 import ast
+import math
 import networkx as nx
-import stscheduler
 
-START_TIME = time.time()
 
+
+OPTION = sys.argv[1]
+OPTION2 = sys.argv[2]
+outout_file_postfix = OPTION
+
+aca_year = OPTION2[1:]
+sem = OPTION2[:1]
+
+semester = sem
+
+academic_year = aca_year
+
+# student regist file path
+regist_path = "data/regist/regist-"+academic_year+"-"+semester+"-cb-sec.in"
+
+# student enrolled courses file path
+st_courses_path = "data/sched-"+academic_year+"-"+semester+"/courses-"+academic_year+"-"+semester+".in"
+
+# all exam courses file path
+all_courses_path = "data/all-exam-courses/all-exam-course.in"
+
+# conflicts file path
+conflicts_path  = "data/sched-"+academic_year+"-"+semester+"/conflicts-"+academic_year+"-"+semester+"-sorted.in"
+
+# courses by faculty folder path
+fa_course_path  = "data/exam-courses-faculty"
+
+# capacity file path
+capacity_path  = "data/capacity/sum-capa-reg.in"
+
+# output folder path
+out_folder_path  = "data/sched-exam-table/"
+
+
+START_TIME = time.time() 
+MAX_CAPACITY = {
+    "01": 0,
+    "02": 0,
+    "03": 0,
+    "04": 0,
+    "05": 0,
+    "06": 0,
+    "07": 0,
+    "08": 0,
+    "09": 0,
+    "10": 0,
+    "11": 0,
+    "12": 0,
+    "13": 0,
+    "14": 0,
+    "15": 0,
+    "16": 0,
+    "17": 0,
+    "18": 0,
+    "19": 0,
+    "20": 0,
+    "21": 0,
+    "RB": 0,
+    "99": math.inf
+}
 # Valid slot [0-41]
 TOTAL_SLOTS = 42
 SLOTS = [slot for slot in range(TOTAL_SLOTS)]
+SLOT_CAPACITY = { s:MAX_CAPACITY.copy() for s in range(TOTAL_SLOTS) }
 
+
+capacity_penalty_value = 0
 # Read all student enroll courses
-with open(stscheduler.regist, "r", encoding="utf-8-sig") as regist:
-    STUDENTS = list(csv.reader(regist, delimiter=" "))
+with open(regist_path, "r", encoding="utf-8-sig") as reg:
+    STUDENTS = list(csv.reader(reg, delimiter=" "))
 
 # Read all courses that students enroll
 std_en_courses_set = set()
 STD_ENROLL_COURSES = {}
-with open(stscheduler.st_courses, "r", encoding="utf-8-sig") as courses:
+with open(st_courses_path, "r", encoding="utf-8-sig") as courses:
     for row in courses:
         std_en_courses_set.add(row.split()[0])
         STD_ENROLL_COURSES[row.split()[0]] = int(row.split()[1])
 
 # Read exam courses from exam table
-exam_courses = set()
+
 # with open("exams-timetbl-63-2.txt", "r") as read_exam_slot:
 #     for row in read_exam_slot:
 #         course = row.split()[0]
 #         exam_courses.add(course)
 
 # Read exam courses from file (no-section)
-with open(stscheduler.all_courses, "r", encoding="utf-8-sig") as courses:
+exam_courses = set()
+with open(all_courses_path, "r", encoding="utf-8-sig") as courses:
     for c in courses:
         cs = c.rstrip("\n")
         exam_courses.add(cs)
+
+
+
+with open(capacity_path , "r", encoding="utf-8-sig") as capa:
+    for row in capa:
+        MAX_CAPACITY[row.split()[0]] = int(row.split()[1])
+
+COURSE_FACULTY = {}
+
+for file in os.listdir(fa_course_path):
+    with open(os.path.join(fa_course_path , file), "r", encoding="utf-8-sig") as courses:
+        for row in courses:
+            COURSE_FACULTY[row.rstrip("\n")] = file.replace(".in", "")
 
 # to_remove = set()
 # for c in std_en_courses_set:
@@ -63,7 +140,7 @@ COURSE_LIST.sort()
 
 TOTAL_COURSES = len(COURSE_LIST)
 
-with open(stscheduler.conflicts, "r", encoding="utf-8-sig") as conflicts:
+with open(conflicts_path, "r", encoding="utf-8-sig") as conflicts:
     CONFLICTS = list(csv.reader(conflicts, delimiter=" "))
 
 CONFLICT_DICT = {}
@@ -98,11 +175,8 @@ SG = [
     for c in sorted(nx.connected_components(G), key=len, reverse=True)
 ]
 
-print("--- Create graph time %s seconds ---" % ((time.time() - START_TIME)))
-# print([len(c) for c in SG])
-# for s in SG:
-#     if len(s) < 5:
-#         print(list(s.nodes))
+# print("--- Create graph time %s seconds ---" % ((time.time() - START_TIME)))
+
 def printProgressBar(
     iteration,
     total,
@@ -161,6 +235,115 @@ def get_slot(course_list, student):
         # Remove student that don't have any exam
         student_slot.append(s_remove_noslot)
     return remove_no_exam(student_slot)
+
+
+def linear_pen(x):
+    return 500 * (x - 80)
+
+
+def expo_pen(x):
+    return (500 * (x - 80)) + (2**(2*((x / 10) - 1))) - (2**18)
+
+def calc_capacity_penalty_for_eng(fa, slot, total_std_in_course, max_capacity):
+    fa_for_eng = ["02","03","04","05","06","08","15","16","18","19","20"]
+    fa_for_eng = sorted(fa_for_eng, key = lambda fa : max_capacity[fa], reverse=True)
+    available_fa = ["01","RB"] + fa_for_eng
+    this_used_fa_capa = {}
+    penalty = 0
+    remain = total_std_in_course
+    for fa in available_fa:
+        used_fa_capa = SLOT_CAPACITY[slot][fa]
+        max80_fa_capa = (max_capacity[fa] * 80) // 100
+    
+        # penalty for eng in same slot
+        if used_fa_capa == max80_fa_capa:
+            penalty += 1
+
+        fa80_remain = max80_fa_capa - used_fa_capa
+        if remain > fa80_remain:
+            this_used_fa_capa[fa] = fa80_remain
+            remain = remain - fa80_remain
+        else:
+            this_used_fa_capa[fa] = remain
+            remain = 0
+            break
+    
+    if remain > 0:
+        for fa in available_fa:
+            max_fa_capa = max_capacity[fa]
+            max80_fa_capa = (max_fa_capa * 80) // 100
+            fa80_100 = max_fa_capa - max80_fa_capa
+            if remain > fa80_100:
+                this_used_fa_capa[fa] += fa80_100
+                remain = remain - fa80_100
+                penalty += linear_pen(100)
+            else:
+                this_used_fa_capa[fa] += remain
+                over80_percent = ((remain + max80_fa_capa) / max_fa_capa) * 100
+                penalty += linear_pen(over80_percent)
+                remain = 0
+                break
+    if remain > 0:
+        max_01_capa = max_capacity["01"]
+        overflow_percent = ((remain + max_01_capa) / max_01_capa) * 100
+        penalty += expo_pen(overflow_percent)
+    return penalty, this_used_fa_capa
+    
+
+def calc_capacity_penalty_v1(fa, slot, total_std_in_course, max_capacity):
+    capacity_penalty = 0
+    fa_penalty = 0
+    rb_penalty = 0
+    used_rb_capa = SLOT_CAPACITY[slot]["RB"]
+    used_fa_capa = SLOT_CAPACITY[slot][fa]
+    max_rb_capa = max_capacity["RB"]
+    max_fa_capa = max_capacity[fa]
+    max80_rb_capa = (max_rb_capa * 80) // 100
+    max80_fa_capa = (max_fa_capa * 80) // 100 
+    this_used_rb_capa = 0
+    this_used_fa_capa = 0
+    rb80_remain = max80_rb_capa - used_rb_capa
+    fa80_remain = max80_fa_capa - used_fa_capa
+    rb80_100 = max_rb_capa - max80_rb_capa
+    fa80_100 = max_fa_capa - max80_fa_capa
+
+    if total_std_in_course > fa80_remain:
+        remain_std = total_std_in_course - fa80_remain
+        if remain_std > rb80_remain:
+            remain_std = remain_std - rb80_remain
+            if remain_std > fa80_100:
+                remain_std = remain_std - fa80_100
+                # over fa80 -> over rb80 -> over fa100 -> over rb100 -> overflow fa100
+                if remain_std > rb80_100:
+                    remain_std = remain_std - rb80_100
+                    overflow_percent = ((remain_std + max_fa_capa) / max_fa_capa) * 100
+                    this_used_fa_capa = fa80_remain + fa80_100
+                    this_used_rb_capa = rb80_remain + rb80_100
+                    fa_penalty = expo_pen(overflow_percent)
+                    rb_penalty = linear_pen(100)
+                # over fa80 -> over rb80 -> over fa100 -> fit in rb80-100
+                else:
+                    over80_rb = ((max80_rb_capa + remain_std) / max_rb_capa) * 100
+                    this_used_fa_capa = fa80_remain + fa80_100
+                    this_used_rb_capa = rb80_remain + remain_std
+                    fa_penalty = linear_pen(100)
+                    rb_penalty = linear_pen(over80_rb)
+            # over fa80 -> over rb80 -> fit in fa80-100
+            else:
+                over80_fa = ((max80_fa_capa + remain_std) / max_fa_capa) * 100
+                this_used_fa_capa = fa80_remain + remain_std
+                this_used_rb_capa = rb80_remain
+                fa_penalty = linear_pen(over80_fa)
+        # over fa80 -> fit in rb80
+        else:
+            this_used_fa_capa = fa80_remain
+            this_used_rb_capa = remain_std
+    # fit in fa80        
+    else:
+        this_used_fa_capa = total_std_in_course
+    capacity_penalty = fa_penalty + rb_penalty
+
+    return capacity_penalty, this_used_fa_capa, this_used_rb_capa
 
 
 def calc_each_penalty(pen_count):
@@ -248,14 +431,16 @@ def count_penalty2(solution, start_slot, end_slot):
     return pen_count
 
 
+
+
 STUDENT_CLEAN = get_slot(COURSE_LIST, STUDENTS)
 STUDENT_DUP_COUNT = {str(s): STUDENT_CLEAN.count(s) for s in STUDENT_CLEAN}
 
 
-print("Total courses:", TOTAL_COURSES)
-print("Total students:", len(STUDENTS))
-print("Total students having an exam:", len(STUDENT_CLEAN))
-print("--- Clean data time %s seconds ---" % ((time.time() - START_TIME)))
+# print("Total courses:", TOTAL_COURSES)
+# print("Total students:", len(STUDENTS))
+# print("Total students having an exam:", len(STUDENT_CLEAN))
+# print("--- Clean data time %s seconds ---" % ((time.time() - START_TIME)))
 
 
 class Schedule(object):
@@ -270,105 +455,146 @@ class Schedule(object):
         self.wait_count = self.count_all_wait_penalty()
         self.penalty_count[7] = self.wait_count
         self.penalty_value = calc_each_penalty(self.penalty_count)
+        self.penalty_value[1] = capacity_penalty_value
         self.total_penalty = self.calc_total_penalty(self.penalty_value)
 
     @classmethod
-    def create_sorted_node(self, g):
+    def create_sorted_node(self, g, option):
         """
         Return tuple of nodes sort by degree -> number of student enrolled -> number of conflicts
         """
         global STD_ENROLL_COURSES, NODE_SUM_CONFLICTS
         g_tuple = []
 
-        for i in list(g.degree):
-            g_tuple.append(
-                (i[1], STD_ENROLL_COURSES[i[0]], NODE_SUM_CONFLICTS.get(i[0], 0), i[0])
-            )
+        if option == "-std":
+            for i in list(g.degree):
+                course = i[0]
+                degree = i[1]
+                g_tuple.append(
+                    (STD_ENROLL_COURSES[course], degree, NODE_SUM_CONFLICTS.get(course, 0), course)
+                )
+        if option == "-deg":
+            for i in list(g.degree):
+                course = i[0]
+                degree = i[1]
+                g_tuple.append(
+                    (degree, STD_ENROLL_COURSES[course], NODE_SUM_CONFLICTS.get(course, 0), course)
+                )
 
         return sorted(g_tuple, reverse=True)
+
+    @classmethod    
+    def sort_nodes_neighbor(self, neighbors, nodes_tuple):
+
+        nodes = []
+        for n in neighbors:
+            for i in range(len(nodes_tuple)):
+                if nodes_tuple[i][3] == n:
+                    nodes.append(nodes_tuple[i])
+
+        sorted_nodes = sorted(nodes, reverse=True)
+        ret_node = [n[3] for n in sorted_nodes]
+        return ret_node
+
+
 
     @classmethod
     def create_schedule(self):
         """
         Find schedule by graph coloring
         """
-        global SG, SLOTS, TOTAL_SLOTS
+        # global SG, SLOTS, TOTAL_SLOTS, MAX_CAPACITY, COURSE_FACULTY, SLOT_CAPACITY, capacity_penalty_value, capacity_penalty_count
+        global SLOT_CAPACITY, capacity_penalty_value
         solution = {}
+        option1 = OPTION[:4]
+        option2 = OPTION[4:]
 
         for g in SG:
-            # tuple (degree, enrolled_amount, sum_conflict)
-            sorted_nodes = self.create_sorted_node(g)
-            degree_dict = dict(g.degree)
-            root = sorted_nodes[0][3]
-            edges = nx.bfs_edges(
-                g,
-                root,
-                sort_neighbors=lambda nodes: sorted(
-                    nodes, key=lambda x: degree_dict[x], reverse=True
-                ),
-            )
-            nodes_seq = [root] + [v for u, v in edges]
+
+            sorted_nodes = self.create_sorted_node(g,option1)
+
+            if option2 == "-bfs":
+                root = sorted_nodes[0][3]
+                edges = nx.bfs_edges(
+                    g,
+                    root,
+                    sort_neighbors=lambda neighbors: self.sort_nodes_neighbor(neighbors,sorted_nodes)
+                )
+                nodes_seq = [root] + [v for u, v in edges]
+            else:
+                nodes_seq = [node for deg, std_en, conf, node in sorted_nodes]
+
             for n in nodes_seq:
                 slots = set([slot for slot in range(TOTAL_SLOTS)])
-                color = set(
-                    [solution[c] for c in set(g.neighbors(n)) if c in solution.keys()]
-                )
+                color = set([solution[c] for c in set(g.neighbors(n)) if c in solution.keys()])
                 slots = slots.difference(color)
+                
+                node_n = n
+                if len(n) > 6:
+                    node_n = n[:-4]
 
+                course_fa = COURSE_FACULTY.get(node_n,"99")
+                total_std_in_course = STD_ENROLL_COURSES[n]
+                used_capa = {}
+                capacity_penalty = {}
                 slot_penalty = {}
+
                 for s in SLOTS:
                     solution[n] = s
                     if s % 3 == 0 and s != 0:
                         penalty_count = count_penalty2(solution, s - 1, s + 2)
                     elif s % 3 == 1:
                         penalty_count = count_penalty2(solution, s - 1, s + 1)
-                    elif s % 3 == 2 and s != 41:
+                    elif s % 3 == 2 and s != TOTAL_SLOTS-1:
                         penalty_count = count_penalty2(solution, s - 2, s + 1)
                     elif s == 0:
                         penalty_count = count_penalty2(solution, s, s + 2)
-                    elif s == 41:
+                    elif s == TOTAL_SLOTS-1:
                         penalty_count = count_penalty2(solution, s - 2, s)
+                    
+                    if node_n == "001101" or node_n == "001102" or node_n == "001201":
+                        capa_pen, all_used_capa = calc_capacity_penalty_for_eng(course_fa, s ,total_std_in_course,MAX_CAPACITY)
+                        used_capa[s] = all_used_capa
+                    else:
+                        capa_pen, used_fa, used_rb = calc_capacity_penalty_v1(course_fa, s, total_std_in_course, MAX_CAPACITY)
+                        # Exclude CITIZENSHIP course -> online exam
+                        if node_n == "140104":
+                            capa_pen = 0
+                            used_fa = 0
+                            used_rb = 0
+                        
+                        used_capa[s] = {}
+                        used_capa[s]["fa"] = used_fa
+                        used_capa[s]["RB"] = used_rb
+                    capacity_penalty[s] = capa_pen
+
                     penalty_value = sum(calc_each_penalty(penalty_count).values())
+                    penalty_value += capacity_penalty[s]
                     slot_penalty[s] = penalty_value
 
-                # calc penalty then random with weight
                 if slots:
-
-                    # select slot
-                    # find list of all slot that this new slot associated with
-                    # calculate penalty from all slot in above list
-                    # if slot % 3 == 0:
-                    #   -1 0 +1 +2
-                    # elif slot % 3 == 1:
-                    #   -1 0 +1
-                    # elif slot % 3 == 2:
-                    #   -2 -1 0 +1
-                    to_remove_slot = set(SLOTS).difference(slots)
-                    for k in to_remove_slot:
+                    for k in color:
                         slot_penalty.pop(k, None)
 
                 lowest_pen_tuple = min(slot_penalty.items(), key=lambda x: x[1])
                 lowest_pen = lowest_pen_tuple[1]
+
                 lowest_pen_slot = [
                     k for k, v in slot_penalty.items() if v == lowest_pen
                 ]
+                slot_sorted = sorted(lowest_pen_slot, key=lambda slot: ((slot+2)%3, slot//3))
+                solution[n] = slot_sorted[0]
 
-                solution[n] = random.choice(lowest_pen_slot)
+                if node_n == "001101" or node_n == "001102" or node_n == "001201":
+                    for fa, capa in used_capa[slot_sorted[0]].items():
+                        SLOT_CAPACITY[slot_sorted[0]][fa] += capa
+                else:
+                    SLOT_CAPACITY[slot_sorted[0]][course_fa] += used_capa[slot_sorted[0]]["fa"]
+                    SLOT_CAPACITY[slot_sorted[0]]["RB"] += used_capa[slot_sorted[0]]["RB"]
 
-                # solution[n] = lowest_pen_tuple[0]
-
-                # if all(pen == 0 for pen in slot_penalty.values()):
-                #     solution[n] = random.choice(list(slots))
-                # else:
-                #     sum_penalty = sum(slot_penalty.values())
-                #     slot_list = []
-                #     prob_list = []
-                #     for k, v in slot_penalty.items():
-                #         slot_list.append(k)
-                #         prob_list.append(sum_penalty-v)
-                #     solution[n] = random.choices(slot_list, weights=prob_list, k=1)[0]
-
-                # solution[n] = random.choice(list(slots))
+                # TODO Check here for count
+                if capacity_penalty[slot_sorted[0]] > 0:
+                    capacity_penalty_value += capacity_penalty[slot_sorted[0]]
 
         return solution
 
@@ -427,15 +653,12 @@ class Schedule(object):
 
 
 def main():
-    total_sol = 10
     schedule = []
-    printProgressBar(0, total_sol, prefix="Progress:", suffix="Complete", length=50)
-    for i in range(total_sol):
-        sol = Schedule.create_schedule()
-        schedule.append(Schedule(sol))
-        printProgressBar(i + 1, total_sol, prefix="Progress:", suffix="Complete", length=50)
-    schedule = sorted(schedule, key=lambda x: x.penalty_count[3])
-    # schedule = sorted(schedule, key=lambda x: x.total_penalty)
+    # printProgressBar(0, total_sol, prefix="Progress:", suffix="Complete", length=50)
+    sol = Schedule.create_schedule()
+    schedule.append(Schedule(sol))
+        # printProgressBar(i + 1, total_sol, prefix="Progress:", suffix="Complete", length=50)
+
 
     print("Total penalty of solution:", schedule[0].total_penalty)
     print("Each penalty value of solution:")
@@ -446,13 +669,14 @@ def main():
         print(str(int(k)) + ": " + str(v))
     print(schedule[0].penalty_count)
 
-    with open(stscheduler.out_folder+"graph-coloring-solution-63-2.txt",
+    with open(out_folder_path +"graph-coloring-solution-"+
+        academic_year+"-"+semester+outout_file_postfix+".txt",
         "a",
         encoding="utf-8-sig",
     ) as ch:
         for k in sorted(schedule[0].solution.keys()):
             ch.write(str(k) + " " + str(schedule[0].solution[k]) + "\n")
-
+    
     # print("Solution saved to 'graph-coloring-solution.txt'...")
     print("--- Execution time %s seconds ---" % ((time.time() - START_TIME)))
 
